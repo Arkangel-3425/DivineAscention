66bee5047ed0f6480f74caa69173ee8f5f64e891
Joshua D'Abreu <JD250657@Falmouth.ac.uk>
2026-04-14 23:47:04 +0100

    Forgor some stuff

--- InfernalEclipseWeaponsDLC.cs
+++ InfernalEclipseWeaponsDLC.cs
@@ -1,31 +1,38 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Terraria.ModLoader;
 
 namespace InfernalEclipseWeaponsDLC
 {
-	public class InfernalEclipseWeaponsDLC : Mod
-	{
-		internal static InfernalEclipseWeaponsDLC Instance;
+    public class InfernalEclipseWeaponsDLC : Mod
+    {
+        internal static InfernalEclipseWeaponsDLC Instance;
 
-		internal Mod calamity = null;
-		internal Mod calamitybardhealer = null;
-		internal Mod ragnarok = null;
-		internal Mod thorium = null;
+        internal Mod calamity = null;
+        internal Mod calamitybardhealer = null;
+        internal Mod ragnarok = null;
+        internal Mod thorium = null;
+
+        public static ModKeybind AbsoluteTVRemotePause;
 
         public override void Load()
         {
-			Instance = this;
+            Instance = this;
 
-			calamity = null;
-			ModLoader.TryGetMod("CalamityMod", out  calamity);
-			calamitybardhealer = null;
-			ModLoader.TryGetMod("CalamityBardHealer", out calamitybardhealer);
-			ragnarok = null;
-			ModLoader.TryGetMod("RagnarokMod", out ragnarok);
-			thorium = null;
-			ModLoader.TryGetMod("ThoriumMod", out thorium);
+            ModLoader.TryGetMod("CalamityMod", out calamity);
+            ModLoader.TryGetMod("CalamityBardHealer", out calamitybardhealer);
+            ModLoader.TryGetMod("RagnarokMod", out ragnarok);
+            ModLoader.TryGetMod("ThoriumMod", out thorium);
+
+            AbsoluteTVRemotePause = KeybindLoader.RegisterKeybind(
+                this,
+                "Absolute TV Remote Pause",
+                "Mouse3"
+            );
+        }
+
+        public override void Unload()
+        {
+            AbsoluteTVRemotePause = null;
         }
     }
 }
