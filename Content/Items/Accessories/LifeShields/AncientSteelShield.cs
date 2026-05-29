776c6e487e174b208d1bbb123b63ccdb40992287
Darian Hughes <darianmhughes7@gmail.com>
2026-05-28 21:33:23 -0500

    ancient steel sword

---
+++ Content/Items/Accessories/LifeShields/AncientSteelShield.cs
@@ -0,0 +1,48 @@
+﻿using CalamityMod.Items;
+using InfernalEclipseWeaponsDLC.Core.Players;
+using SOTS.Items.AbandonedVillage;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ThoriumMod;
+
+namespace InfernalEclipseWeaponsDLC.Content.Items.Accessories.LifeShields
+{
+    [JITWhenModsEnabled("SOTS")]
+    [ExtendsFromMod("SOTS")]
+    //[AutoloadEquip(EquipType.Shield)]
+    public class AncientSteelShield : ModItem
+    {
+        public override bool IsLoadingEnabled(Mod mod) => WeaponConfig.Instance.LifeShields;
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 19;
+            Item.height = 20;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = CalamityGlobalItem.RarityBlueBuyPrice;
+            Item.accessory = true;
+            Item.defense = 1;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<ThoriumPlayer>().MetalShieldMax += 30;
+            player.GetModPlayer<SOTSCurseVoidEffects>().ancientShieldEquipped = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<AncientSteelBar>(10)
+                .AddIngredient<CharredWood>(10)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
