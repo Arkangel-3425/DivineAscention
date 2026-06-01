a76da21b019595caded3b80ebd12b7cbe5d4f083
Darian Hughes <darianmhughes7@gmail.com>
2026-05-31 22:02:38 -0500

    terra sheath and flail lore reformat

--- Content/Items/Accessories/Melee/Flails/DoubleFlail.cs
+++ Content/Items/Accessories/Melee/Flails/DoubleFlail.cs
@@ -5,6 +5,9 @@ using CalamityMod.Items.Materials;
 using ThoriumMod.Items.BasicAccessories;
 using InfernalEclipseWeaponsDLC.Core.NewFolder;
 using CalamityMod.Items;
+using System.Collections.Generic;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
 
 namespace InfernalEclipseWeaponsDLC.Content.Items.Accessories.Melee.Flails
 {
@@ -20,10 +23,18 @@ namespace InfernalEclipseWeaponsDLC.Content.Items.Accessories.Melee.Flails
             Item.rare = ItemRarityID.Lime;
             Item.accessory = true;
         }
+
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetModPlayer<InfernalWeaponsPlayer>().doubleFlailAcc = true;
         }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Lore lines
+            tooltips.Add(new TooltipLine(Mod, "Lore", Language.GetTextValue("Mods.InfernalEclipseWeaponsDLC.ItemTooltip.DoubleFlailLore")) { OverrideColor = Color.MediumPurple });
+        }
+
         public override void AddRecipes()
         {
             CreateRecipe()
---
+++ Core/Graphics/Metaballs/TerraMetaball.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CalamityMod.Enums;
+using CalamityMod.Graphics.Metaballs;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace InfernalEclipseWeaponsDLC.Core.Graphics.Metaballs
+{
+    public class TerraMetaball : Metaball
+    {
+        public class Particle
+        {
+            public float Size;
+            public Vector2 Velocity;
+            public Vector2 Center;
+            public Texture2D TextureToUse = null;
+            public float rotation = 0f;
+            public Vector2 Scale = Vector2.One;
+
+            public float SizeScaling = 0.85f;
+            public int CurrentFrame = 0;
+            public int MaxFrames = 1;
+
+            public int ShrinkDelay = 45;
+            public int TimeAlive = 0;
+
+            public Particle(Vector2 center, Vector2 velocity, float size)
+            {
+                Center = center;
+                Velocity = velocity;
+                Size = size;
+            }
+
+            public void Update()
+            {
+                TimeAlive++;
+                Center += Velocity;
+                Velocity *= 0.96f;
+
+                if (ShrinkDelay < TimeAlive)
+                    Size *= SizeScaling;
+            }
+        }
+
+        public override bool FixedToScreen => false;
+
+        public static List<Particle> Particles { get; private set; } = new();
+
+        public override bool AnythingToDraw => Particles.Any();
+
+        public static Asset<Texture2D> LayerAsset { get; private set; }
+
+        public override IEnumerable<Texture2D> Layers
+        {
+            get
+            {
+                yield return LayerAsset.Value;
+            }
+        }
+
+        public override void Load()
+        {
+            if (Main.dedServ)
+                return;
+
+            LayerAsset = ModContent.Request<Texture2D>("CalamityMod/Graphics/Metaballs/BloodLayer", AssetRequestMode.ImmediateLoad);
+        }
+
+        public override GeneralDrawLayer DrawLayer => GeneralDrawLayer.BeforeProjectiles;
+
+        public override Color EdgeColor => new Color(0, 255, 140);
+
+        public override void Update()
+        {
+            for (int i = 0; i < Particles.Count; i++)
+                Particles[i].Update();
+
+            Particles.RemoveAll(p => p.Size <= 2f);
+        }
+
+        public static Particle SpawnParticle(Vector2 position, Vector2 velocity, float size)
+        {
+            Particle particle = new(position, velocity, size);
+            Particles.Add(particle);
+            return particle;
+        }
+
+        public override Vector2 CalculateManualOffsetForLayer(int layerIndex)
+        {
+            return Vector2.UnitX * Main.GlobalTimeWrappedHourly * 0.0005f;
+        }
+
+        public override void DrawInstances()
+        {
+            Texture2D tex = ModContent.Request<Texture2D>("CalamityMod/ExtraTextures/BasicCircle").Value;
+
+            foreach (Particle particle in Particles)
+            {
+                Texture2D texture2d = particle.TextureToUse ?? tex;
+                Vector2 drawPosition = particle.Center - Main.screenPosition;
+
+                Rectangle frame = texture2d.Frame(1, particle.MaxFrames, 0, particle.CurrentFrame);
+                Vector2 origin = frame.Size() * 0.5f;
+
+                Vector2 scale = particle.Scale * particle.Size / texture2d.Width;
+
+                float colorInterpolant = (float)(Math.Sin(Main.GlobalTimeWrappedHourly * 5f + particle.TimeAlive * 0.08f) * 0.5f + 0.5f);
+
+                Color terraBladeColor = Color.Lerp(new Color(0, 255, 120), new Color(80, 255, 255),colorInterpolant);
+
+                Main.spriteBatch.Draw( texture2d, drawPosition, frame, terraBladeColor, particle.rotation, origin, scale, SpriteEffects.None, 0f);
+            }
+        }
+    }
+}
